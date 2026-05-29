1345524c754f6e84389135427986bf76fac96a12
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-05-29 22:52:44 +0500

    Remove unnecessary files

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -4,7 +4,6 @@ import patientsRouter from "./src/routes/patients.ts";
 import cors from "cors";
 
 const app = express();
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 app.use(express.json());
 
